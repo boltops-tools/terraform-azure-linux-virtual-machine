4ab1b171b808af094e32dda4a3db6b8dbd45e59e
Tung Nguyen <tongueroo@gmail.com>
2021-11-29 02:21:59 +0000

    update vnet module to point to terraform registry

--- examples/linux_vm/main.tf
+++ examples/linux_vm/main.tf
@@ -8,17 +8,15 @@ module "linux_vm" {
 }
 
 module "vnet" {
-  source = "../../modules/network"
+  source = "boltops-tools/network/azure"
 
   resource_group_name = var.resource_group_name
   subnets = {
     "app" = {
       address_prefixes = ["10.0.0.0/24"]
-      service_endpoints = ["Microsoft.Sql", "Microsoft.Storage"]
     }
     "db" = {
       address_prefixes = ["10.0.1.0/24"]
-      enforce_private_link_endpoint_network_policies = false
     }
   }
 
